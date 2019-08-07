9679683967006ce938db00bfcc9f53e72414ba36
memele <50452427+EmilyPasqualotto@users.noreply.github.com>
2019-08-07 10:30:23 -0300

    Update tofhanoi final file - Copia.py

--- tower of hanoi/tofhanoi final file - Copia.py
+++ tower of hanoi/tofhanoi final file - Copia.py
@@ -10,9 +10,11 @@ t3=[]
 #printar as torres-------------------------------------#
 def print_torres():
    print ('║════════════════║')
-   print (' T1','=',t1,)
-   print (' T2','=',t2,)
-   print (' T3','=',t3,)
+   print ('║T1','=',t1,)
+   print ('║═')
+   print ('║T2','=',t2,)
+   print ('║═')
+   print ('║T3','=',t3,)
    print ('║════════════════║')
 print_torres()
 
@@ -24,32 +26,46 @@ while (t3)!=[3,2,1]:
     inp=inp.split('-')
     house1=inp[0]
     house2=inp[1]
-        
+    
     if (house1)==('1'):
-        a=t1[(len(t1)-1)]
-        t1.pop(len(t1)-1)
-        if (house2)==('2'):
-            t2.append((a))
-        elif (house2)==('3'):
-            t3.append((a))
+       if (t1!=[]):
+           a=t1[(len(t1)-1)]
+           t1.pop(len(t1)-1)
+           if (house2)==('2'):
+               t2.append((a))
+               print_torres()
+           elif (house2)==('3'):
+               t3.append((a))
+               print_torres()
+           else:
+               print('think and try again')
 
     elif (house1)==('2'):
-        a=t2[(len(t2)-1)]
-        t2.pop(len(t2)-1)
-        if (house2)==('1'):
-            t1.append((a))
-        elif (house2)==('3'):
-            t3.append((a))
+        if (t2!=[]):
+           a=t2[(len(t2)-1)]
+           t2.pop(len(t2)-1)
+           if (house2)==('1'):
+               t1.append((a))
+               print_torres()
+           elif (house2)==('3'):
+               t3.append((a))
+               print_torres()
+        else:
+            print('think and try again')
 
     elif (house1)==('3'):
-        a=t3[(len(t3)-1)]
-        t3.pop(len(t3)-1)
-        if (house2)==('2'):
-            t2.append((a))
-        elif (house2)==('1'):
-            t1.append((a))
-   
-    print_torres()
+        if (t3!=[]):
+           a=t3[(len(t3)-1)]
+           t3.pop(len(t3)-1)
+           if (house2)==('2'):
+               t2.append((a))
+               print_torres()
+           elif (house2)==('1'):
+               t1.append((a))
+               print_torres()
+        else:
+            print('think and try again :p')
+
     print('')
 
 #depois de sair do loop, o programa finaliza e mostra--#
